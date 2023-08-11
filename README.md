ac7094f7325131b69e5607b576495d01a5702c50
Archit Madankar <architmadankar21@gmail.com>
2023-08-11 13:30:11 +0530

    Final Upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Muujik - A better Spotify 
+
+Muujik - A better Spotify, That uses Shazam API to fetch Songs
+
+## 🚀 About us
+* [Archit Madankar](https://github.com/architmadankar/) Backend and API Integration
+* [Dnyanada Khandait](https://github.com/saik20012/) Frontend and Backend Integration
+* [Anushka Shivanand](https://github.com/Carfreak30/) Frontend Design
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import VITE_SHAZAM_CORE_RAPID_API_KEY from '../../../.env.example';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', `${VITE_SHAZAM_CORE_RAPID_API_KEY}`);
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+  useGetSongByCountryQuery,
+  useGetSongBySearchQuery,
+} = shazamCoreApi;
